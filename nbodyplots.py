1333448657c29e5bce23b0b54beb7c55d0e09a76
mef51 <hms.mohammed@gmail.com>
2017-02-28 10:44:13 -0500

    animate slowly, but it works

--- nbodyplots.py
+++ nbodyplots.py
@@ -14,31 +14,28 @@ planets = simulation['planets'] # first index is planet id,
 # The indexing works like this: planets[planetid, posOrVel, time, component]
 
 # plot an animation
+framesdir = '__frames'
+if not os.path.exists(framesdir):
+	os.mkdir(framesdir)
+
+for i, t in enumerate(time):
+	filename = os.path.join(framesdir, 'frame' + str(i) + '.png')
+	currplot = {
+		'filename': filename,
+		'ylim': (-1, 1),
+		'xlim': (-2,2),
+	}
+
+	for n, _ in enumerate(planets):
+		currplot[n] = {'x': planets[n,0,i,0], 'y': planets[n,0,i,1], 'line': 'ko'}
+
+	if i % 50 == 0:
+		print('frame' + str(i) + '...')
+	plawt.plot(currplot)
+
 with imageio.get_writer('figure8.mp4', mode='I', fps=20) as writer:
-	for i, t in enumerate(time):
-		currplot = {
-			# 'filename': os.path.join(framesdir, 'frame' + str(i) + '.png'),
-			'ylim': (-1, 1),
-			'xlim': (-2,2),
-			'keepOpen': True
-		}
+	for filename in os.listdir(framesdir):
+		image = imageio.imread(os.path.join(framesdir, filename))
+		writer.append_data(image)
 
-		for n, _ in enumerate(planets):
-			currplot[n] = {'x': planets[n,0,i,0], 'y': planets[n,0,i,1], 'line': 'ko'}
-
-		if i % 50 == 0:
-			print('frame' + str(i) + '...')
-		plt = plawt.plot(currplot)
-		fig = plt.gcf()
-		fig.canvas.draw()
-		frame = fig.canvas.tostring_rgb()
-		# print(type(frame))
-		writer.append_data(imageio.imread(str(frame)))
-		plt.close()
-
-# with imageio.get_writer('figure8.mp4', mode='I', fps=20) as writer:
-# 	for image in images:
-#  		writer.append_data(image)
-	# for filename in os.listdir(framesdir):
-	#     image = imageio.imread(os.path.join(framesdir, filename))
-	#     writer.append_data(image)
+shutil.rmtree(framesdir)
