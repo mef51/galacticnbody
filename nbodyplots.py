0c060a9a31331bb252454a4f6a9115eca257c7ba
mohammed chamma <hms.mohammed@gmail.com>
2017-03-01 03:15:13 -0500

    shunt things around

--- nbodyplots.py
+++ nbodyplots.py
@@ -5,34 +5,6 @@ import numpy as np
 import plawt, imageio
 from parsenbody import parseNBodyData
 
-# plot an animation
-def saveanimation():
-	framesdir = '__frames'
-	if not os.path.exists(framesdir):
-		os.mkdir(framesdir)
-
-	for i, t in enumerate(time):
-		filename = os.path.join(framesdir, 'frame' + str(i) + '.png')
-		currplot = {
-			'filename': filename,
-			'ylim': (-1, 1),
-			'xlim': (-2,2),
-		}
-
-		for n, _ in enumerate(planets):
-			currplot[n] = {'x': planets[n,0,i,0], 'y': planets[n,0,i,1], 'line': 'ko'}
-
-		if i % 50 == 0:
-			print('frame' + str(i) + '...')
-		plawt.plot(currplot)
-
-	with imageio.get_writer('figure8.mp4', mode='I', fps=20) as writer:
-		for filename in os.listdir(framesdir):
-			image = imageio.imread(os.path.join(framesdir, filename))
-			writer.append_data(image)
-
-	shutil.rmtree(framesdir)
-
 def threebodyplots():
 	simdir = 'simulations'
 	figdir = 'figures'
@@ -113,5 +85,33 @@ plawt.plot({
 	'show': False
 })
 
-# threebodyplots()
+# plot an animation
+def saveanimation():
+	framesdir = '__frames'
+	if not os.path.exists(framesdir):
+		os.mkdir(framesdir)
+
+	for i, t in enumerate(time):
+		filename = os.path.join(framesdir, 'frame' + str(i) + '.png')
+		currplot = {
+			'filename': filename,
+			'ylim': (-1, 1),
+			'xlim': (-2,2),
+		}
+
+		for n, _ in enumerate(planets):
+			currplot[n] = {'x': planets[n,0,i,0], 'y': planets[n,0,i,1], 'line': 'ko'}
+
+		if i % 50 == 0:
+			print('frame' + str(i) + '...')
+		plawt.plot(currplot)
+
+	with imageio.get_writer('figure8.mp4', mode='I', fps=20) as writer:
+		for filename in os.listdir(framesdir):
+			image = imageio.imread(os.path.join(framesdir, filename))
+			writer.append_data(image)
+
+	shutil.rmtree(framesdir)
+
+threebodyplots()
 # saveanimation()
